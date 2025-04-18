3991c1ee59fa26fb52c09ac41f0ebe78224736e0
Giuliano87 <giulianojacobmatosonishi@gmail.com>
2025-04-18 13:27:51 +0900

    Jacob

--- Script/main.py
+++ Script/main.py
@@ -1,36 +1,12 @@
-x = y = z = 10
+# Faça o login no sistema
 
-x = 20
-y = 30
-z = 40
 
-print("A soma dos dois valores é:", x + y)
-print("A soma dos três valores é:", x + y + z)
+n = input("Digite sua conta: ")
+print("Sua conta é: ", n)
+# Verifique se o número é par ou ímpar
 
-print("O produto dos dois valores é:", x * y)
-print("O produto dos três valores é:", x * y * z)
-
-print("A divisão dos dois valores é:", x / y)
-print("A divisão dos três valores é:", x / y / z)
-
-print("A potência dos dois valores é:", x ** y)
-print("A potência dos três valores é:", x ** y ** z)    
-print("O resto da divisão dos dois valores é:", x % y)
-print("O resto da divisão dos três valores é:", x % y % z)
-print("O maior valor é:", max(x, y, z))
-print("O menor valor é:", min(x, y, z))
-print("A média dos três valores é:", (x + y + z) / 3)
-print("O desvio padrão dos três valores é:", (x - y - z) ** 0.5)
-print("O valor absoluto do número é:", abs(x))
-print("O valor inteiro do número é:", int(x))
-print("O valor float do número é:", float(x))   
-# The output of the code will be:
-# A soma dos dois valores é: 50
-# A soma dos três valores é: 100
-# O produto dos dois valores é: 600
-# O produto dos três valores é: 24000
-# A divisão dos dois valores é: 0.6666666666666666
-# A divisão dos três valores é: 0.0041666666666666664
-# A potência dos dois valores é: 10000000000
-# A potência dos três valores é: 1.0000000000000002e+24
-# O resto da divisão dos dois valores é: 0
+n = int(input("Digite um número: "))
+if n % 2 == 0:
+    print("O número é par")
+else:
+    print("O número é ímpar")   
